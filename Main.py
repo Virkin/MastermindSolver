15de5e81cc565dea028859a9586c0798c243ebde
Virkin <virgile.prin@gmail.com>
2024-01-04 14:13:41 +0100

    Add checks for the input given by the player

--- Main.py
+++ Main.py
@@ -1,9 +1,31 @@
 from Mastermind import Mastermind
 
+gNumberOfColors = 5
+gCodeLength = 3
+gNumberOfTry = 12
+
+def CheckInput( pInput ) :
+    lIsInputCorrect = True
+    
+    if not pInput.isdigit() :
+        print("Only number is accepted")
+        lIsInputCorrect = False
+    
+    if not len( pInput ) == gCodeLength :
+        print("Code length must be exactly {} colors".format( gCodeLength ) )
+        lIsInputCorrect = False
+
+    return lIsInputCorrect
+
 if __name__ == "__main__":
-    lMastermind = Mastermind( 8, 5, 12 )
+    lMastermind = Mastermind( gNumberOfColors, gCodeLength, gNumberOfTry )
     lMastermind.InitializeGame()
 
     while not ( lMastermind.IsSolved() or lMastermind.IsAllTryUsed() ) :
-        lInputCode = input()
+        lCorrectInputGiven = False
+
+        while not lCorrectInputGiven :
+            lInputCode = input()
+            lCorrectInputGiven = CheckInput( lInputCode )
+
         print( lMastermind.CheckCode( list( map( int, list( lInputCode ) ) ) ) )
